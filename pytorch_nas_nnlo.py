5828664d88d64a096c00ce058ce5e4e6bd008e2b
Irena Veljanovic <irena.veljanovic@cern.ch>
2022-10-31 12:51:24 +0100

    Initial idea for NAS class

--- pytorch_nas_nnlo.py
+++ pytorch_nas_nnlo.py
@@ -10,13 +10,19 @@ from optuna.trial import TrialState
 
 
 input_size = 784
-# hidden_size = 100
 n_classes = 10
 batch_size = 8
 n_epochs = 10
 device = torch.device("cuda")
 N_TRAIN_EXAMPLES = batch_size*40
 N_VALID_EXAMPLES = batch_size * 20
+
+def get_data(batch_size):
+    train_dataset = torchvision.datasets.MNIST(root = "./data", train = True, transform = transforms.ToTensor() ,download = True)
+    test_dataset = torchvision.datasets.MNIST(root = "./data", train = False, transform = transforms.ToTensor())
+    train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size ,shuffle = True)
+    test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
+    return train_loader, test_loader
 
 class NeuralNet(nn.Module):
     def __init__(self, trial, input_size, hidden_size, num_classes):
@@ -30,88 +36,92 @@ class NeuralNet(nn.Module):
         out = self.l2(out)
         return out
 
-def define_model(trial):
+
+def define_model(trial, param):
     # We optimize the number of layers, hidden units and dropout ratio in each layer.
-    hidden_size = trial.suggest_int("hidden_size", 50, 100)
-    return NeuralNet(trial, input_size, hidden_size, n_classes)
+    return NeuralNet(trial, input_size, param, n_classes)
 
-def get_data(batch_size):
-    train_dataset = torchvision.datasets.MNIST(root = "./data", train = True, transform = transforms.ToTensor() ,download = True)
-    test_dataset = torchvision.datasets.MNIST(root = "./data", train = False, transform = transforms.ToTensor())
-    train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size ,shuffle = True)
-    test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
-    return train_loader, test_loader
+class NeuralArchitectureOptimizer():
+    def __init__(self, train_loader, test_loader, model_fcn, architecture_parameters) -> None:
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.model_fcn = model_fcn
+        self.architecture_parameters = architecture_parameters
 
+    def create_architecture(self, trial):
+        for architecture_param in self.architecture_parameters.keys():
+            param = trial.suggest_int(architecture_param, self.architecture_parameters[architecture_param][0], self.architecture_parameters[architecture_param][1])
+        model = self.model_fcn(trial, param)
+        return model
+        
+    def objective(self, trial):
+        criterion =  nn.CrossEntropyLoss()
 
-def objective(trial):
-    criterion =  nn.CrossEntropyLoss()
+        model = self.create_architecture(trial).to(device)
+        optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+        
+        # Training loop
+        for epoch in range (n_epochs):
+            for i, (images, labels) in enumerate(self.train_loader):
+                # Limiting training data for faster epochs.
+                if i * batch_size >= N_TRAIN_EXAMPLES:
+                    break
 
-    model = define_model(trial).to(device)
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    train_loader, test_loader = get_data(batch_size)
+                images = images.reshape(-1, 28*28).to(device)
+                labels = labels.to(device)
 
-    # optimizer = self.create_optimizer(trial)
-    
-    # Training loop
-    for epoch in range (n_epochs):
-        for i, (images, labels) in enumerate(train_loader):
-            # Limiting training data for faster epochs.
-            if i * batch_size >= N_TRAIN_EXAMPLES:
-                break
+                #forward
+                outputs = model(images)
+                loss = criterion(outputs, labels)
 
-            images = images.reshape(-1, 28*28).to(device)
-            labels = labels.to(device)
+                #backwards
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-            #forward
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+        with torch.no_grad():
+            n_correct = 0
+            n_samples = 0
+            for i, (images, labels) in enumerate(self.test_loader):
+                # Limiting validation data.
+                if i * batch_size >= N_VALID_EXAMPLES:
+                    break
+                images = images.reshape(-1, 28*28).to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                _, predictions = torch.max(outputs, 1) #returns values of max and its indes // WE ARE INTERESTED IN INDICES
+                n_samples +=  labels.shape[0]
+                n_correct += (predictions == labels).sum().item()
+            acc = 100 * n_correct/n_samples
 
-            #backwards
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        return acc
 
-    with torch.no_grad():
-        n_correct = 0
-        n_samples = 0
-        for i, (images, labels) in enumerate(test_loader):
-            # Limiting validation data.
-            if i * batch_size >= N_VALID_EXAMPLES:
-                break
-            images = images.reshape(-1, 28*28).to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            _, predictions = torch.max(outputs, 1) #returns values of max and its indes // WE ARE INTERESTED IN INDICES
-            n_samples +=  labels.shape[0]
-            n_correct += (predictions == labels).sum().item()
-        acc = 100 * n_correct/n_samples
+    def study_and_optimize(self):
+        study = optuna.create_study(direction="maximize", study_name="nas-trial-nnlo")
+        study.optimize(self.objective, n_trials=30, timeout=600)
 
-    return acc
+        pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
+        complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
 
-def study_and_optimize():
-    study = optuna.create_study(direction="maximize", study_name="pytorch-trial-nnlo")
-    study.optimize(objective, n_trials=30, timeout=600)
+        print("Study statistics: ")
+        print("  Number of finished trials: ", len(study.trials))
+        print("  Number of pruned trials: ", len(pruned_trials))
+        print("  Number of complete trials: ", len(complete_trials))
 
-    pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
-    complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
+        print("Best trial:")
+        trial = study.best_trial
 
-    print("Study statistics: ")
-    print("  Number of finished trials: ", len(study.trials))
-    print("  Number of pruned trials: ", len(pruned_trials))
-    print("  Number of complete trials: ", len(complete_trials))
+        print("  Value: ", trial.value)
 
-    print("Best trial:")
-    trial = study.best_trial
-
-    print("  Value: ", trial.value)
-
-    print("  Params: ")
-    for key, value in trial.params.items():
-        print("    {}: {}".format(key, value))
+        print("  Params: ")
+        for key, value in trial.params.items():
+            print("    {}: {}".format(key, value))
 
 
 if __name__ == "__main__":
 
-    study_and_optimize()
+    train_loader, test_loader = get_data(batch_size)
+    
+    nn_architecture_search = NeuralArchitectureOptimizer(train_loader, test_loader, define_model, architecture_parameters={'hidden_size': [90, 100]} )
 
-    # nn_architecture_search = ... TODO: Make a class for NAS
+    nn_architecture_search.study_and_optimize()
